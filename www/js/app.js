0eb4c7af5553a8b7d88bb6b3af411a6e4f66352e
astro2linus <astro2linus@gmail.com>
2015-02-28 09:20:55 +0800

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,117 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+
+const host = 'http://192.168.199.201';
+
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider, $compileProvider) {
+
+  $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|file|itms-services):/);
+
+  $stateProvider.state('products', {
+    url: '/products',
+    controller: 'ProductsCtrl',
+    templateUrl: 'templates/products.html'
+  })
+  .state('product', {
+    url: '/products/:id',
+    controller: 'ProductCtrl',
+    templateUrl: 'templates/product.html'
+  })
+
+  $urlRouterProvider.otherwise('/products');
+})
+
+.controller('ProductsCtrl', function($scope, products, $ionicLoading) {
+
+  $scope.host = host;
+
+  $scope.refreshProducts = function() {
+    products.load().then(function(){
+      $scope.products = products.list;
+      $scope.$broadcast('scroll.refreshComplete');
+    })
+  }
+
+  $ionicLoading.show({
+    template: '<i class="ion-loading-c"></i><br>Loading ...'
+  });
+
+  products.ready.then(function(){
+    $scope.products = products.list;
+    $ionicLoading.hide();
+  });
+
+})
+
+.controller('ProductCtrl', function($scope, productReleases, $stateParams, $ionicLoading){
+
+  $scope.refreshReleases = function() {
+    productReleases.load($stateParams.id).then(function(){
+      $scope.releases = productReleases.list;
+      $scope.$broadcast('scroll.refreshComplete');
+    })
+  }
+
+
+  $ionicLoading.show({
+    template: '<i class="ion-loading-c"></i><br>Loading ...'
+  });
+
+
+  productReleases.load($stateParams.id).then(function(){
+    $scope.releases = productReleases.list;
+    $ionicLoading.hide();
+  })
+})
+
+.factory('products', function($http) {
+  var products = {};
+  products.list = [];
+  products.load = function() {
+    return $http.get(host + '/api/products')
+      .then(function(res) {
+        products.list = res.data;
+      });
+  };
+
+  products.ready = products.load();
+
+  return products;
+})
+
+.factory('productReleases', function($http) {
+  var releases = {};
+  releases.list = [];
+  releases.load = function(product_id) {
+    return $http.get(host + '/api/products/' + product_id + '/releases')
+      .then(function(res) {
+        releases.list = res.data;
+      });
+  };
+  return releases;
+})
+
+
+
+
+
+
+
